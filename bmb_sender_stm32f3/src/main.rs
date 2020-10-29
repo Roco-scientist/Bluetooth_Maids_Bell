9c65c5d441d7d5d2f3cb1a31cb25c33c20b499b9
Rory Coffey <coffeyrt@gmail.com>
2020-10-28 22:19:21 -0400

    message sends easily, button doesn't yet work

--- bmb_sender_stm32f3/src/main.rs
+++ bmb_sender_stm32f3/src/main.rs
@@ -1,6 +1,9 @@
 #![no_main]
 #![no_std]
 
+#[macro_use(block)]
+extern crate nb;
+
 #[cfg(debug_assertions)]
 use panic_itm as _;
 
@@ -22,7 +25,10 @@ fn main() -> ! {
 
     // usart1 is on apb2enr bus.  Writing and enabling usart1 enable bit
     let mut gpioa = peripherals.GPIOA.split(&mut rcc.ahb);
-    // let mut gpioe = peripherals.GPIOE.split(&mut rcc.ahb);
+    let mut gpiob = peripherals.GPIOB.split(&mut rcc.ahb);
+    let button = gpiob
+        .pb2
+        .into_pull_down_input(&mut gpiob.moder, &mut gpiob.pupdr);
     let usart1_txd = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let usart1_rxd = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
 
@@ -36,18 +42,33 @@ fn main() -> ! {
     let (mut usart1_tx, mut usart1_rx) = usart1.split();
     let mut tim6 =
         stm32f3xx_hal::timer::Timer::tim6(peripherals.TIM6, 8.mhz(), clocks, &mut rcc.apb1);
-    // loop {
-    //      usart1_tx.write(2u8).unwrap();
-    //      usart1_tx.bflush().unwrap();
-    //     tim6.wait().unwrap();
-    //  }
+    tim6.start(8.mhz());
+    block!(tim6.wait());
     let mut recieved = false;
     loop {
-        if let Ok(byte) = usart1_rx.read() {
+        if button.is_low().unwrap() {
+            usart1_tx.bwrite_all(&b"LOW"[..]).unwrap();
+            usart1_tx.bflush().unwrap();
+        } else {
+            if button.is_high().unwrap() {
+                for byte in b"HIGH" {
+                    usart1_tx.write(*byte).unwrap();
+                }
+            } else {
+                for byte in b"NONE" {
+                    usart1_tx.write(*byte).unwrap();
+                }
+            }
+        }
+        while button.is_low().unwrap() {}
+        usart1_tx.bwrite_all(&b"BUZZ"[..]).unwrap();
+        usart1_tx.bflush().unwrap();
+        if let Ok(_) = usart1_rx.read() {
             recieved = true;
-            usart1_tx.write(byte).unwrap();
         }
         if recieved {
+            usart1_tx.bwrite_all(&b"RECEIVED"[..]).unwrap();
+            usart1_tx.bflush().unwrap();
             recieved = false
         }
     }
