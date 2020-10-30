45737bd024c2318874f052f7334cb6a9a4206cf7
Rory Coffey <coffeyrt@gmail.com>
2020-10-30 09:25:33 -0400

    loop added along with connecting to rfcomm0

--- bmb_receiver_raspberry_pi/README.md
+++ bmb_receiver_raspberry_pi/README.md
@@ -1,3 +1,8 @@
+# Bluetooth Maids Buzzer receiver for raspberry pi 0 w
+## Required
+Raspberry pi 0 W: <a href=https://www.adafruit.com/product/3400>Buy here</a><rb>
+Rust needs to be installed to build<br>
+
 ## Bluetooth connection
 Check status<br>
 `service bluetooth status`<br>
@@ -8,7 +13,7 @@ Turn on if it is not on<br>
 First edit Bluetooth service in `/etc/systemd/system/dbus-org.bluez.service`<br>
 Add the following lines<br>
 ```
-ExecStart=/usr/lib/bluetooth/bluetoothd -C
+ExecStart=/usr/lib/bluetooth/bluetoothd -C --noplugin=sap
 ExecStartPost=/usr/bin/sdptool add SP
 ```
 `sudo systemctl daemon-reload`<br>
@@ -16,16 +21,28 @@ ExecStartPost=/usr/bin/sdptool add SP
 Edit `/etc/modules-load.d/modules.conf` to load rfcomm module automatically<br>
 Add the following line<br>
 `rfcomm`<br>
-reboot
 
 ### Connect
+Find the mac address of the bluetooth from the microcontroller and pair/trust
 `sudo bluetoothctl`<br>
-`power on`<br>
-`agent on`<br>
-`scan on`<br>
-`scan off`<br>
-`pair <dev>`<br>
-`trust <dev>`<br>
-`quit`<br>
+```
+power on
+agent on
+scan on
+scan off
+pair <mac address>
+trust <mac address>
+quit
+```
+To bind device:<br>
+`sudo rfcomm bind 0 <mac address>`<br>
+To bind device on boot, `edit /etc/rc.local` and add:<br>
+`rfcomm bind 0 <mac address>`<br>
+reboot
 
-`sudo rfcomm bind rfcomm0 <mac address>`
+## Build and run
+Build<br>
+`cargo build --release`<br>
+Run<br>
+`./target/release/bmb_receiver_raspberry_pi`
+
--- bmb_receiver_raspberry_pi/src/main.rs
+++ bmb_receiver_raspberry_pi/src/main.rs
@@ -3,8 +3,9 @@ use std::time;
 
 fn main() {
     // create bluetooth uart connection with no parity, 8 bits data, and 1 bit stopping
+    // rfcomm0 is the bound address of HC-05 from the microcontroller
     let mut bluetooth =
-        uart::Uart::with_path("/dev/ttyACM0", 9600, uart::Parity::None, 8, 1).unwrap();
+        uart::Uart::with_path("/dev/rfcomm0", 9600, uart::Parity::None, 8, 1).unwrap();
 
     // setup the bluetooth to pause waiting for a read with a minimum of 4 bits
     bluetooth
@@ -14,18 +15,20 @@ fn main() {
     // setup blocking write so writing is always completed
     bluetooth.set_write_mode(true).unwrap();
 
-    // create empty data array to put read data into
-    let mut data = [0u8; 32usize];
+    loop{
+        // create empty data array to put read data into
+        let mut data = [0u8; 32usize];
 
-    // Wait for signal to come from sender and put read into data array
-    bluetooth.read(&mut data).unwrap();
+        // Wait for signal to come from sender and put read into data array
+        bluetooth.read(&mut data).unwrap();
 
-    // print for debugging
-    println!("{:?}", data);
+        // print for debugging
+        println!("{:?}", data);
 
-    // return a signal of receipt and confirmation
-    bluetooth.write(&b"RECEIVED"[..]).unwrap();
+        // return a signal of receipt and confirmation
+        bluetooth.write(&b"RECEIVED"[..]).unwrap();
 
-    // flush anything else remaining in the buffers
-    bluetooth.flush(uart::Queue::Both).unwrap();
+        // flush anything else remaining in the buffers
+        bluetooth.flush(uart::Queue::Both).unwrap();
+    }
 }
