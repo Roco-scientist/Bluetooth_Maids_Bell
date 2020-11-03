933e3cf7d6f4d529c47207171f3798c69248805c
Rory Coffey <coffeyrt@gmail.com>
2020-11-03 13:18:37 -0500

    first steps of concurrency.  Needs work on interrupt

--- bmb_receiver_stm32f401/src/main.rs
+++ bmb_receiver_stm32f401/src/main.rs
@@ -7,86 +7,94 @@ use panic_itm as _;
 #[cfg(not(debug_assertions))]
 use panic_abort as _;
 
-use cortex_m_rt::entry;
+use rtic::app;
 use stm32f4xx_hal::{
     block,
     delay::Delay,
-    gpio,
+    gpio::{gpiob::PB9, Output, PushPull},
     prelude::*,
-    serial::{config, Serial},
+    serial::{config, Rx, Serial, Tx},
     stm32, time,
 };
 
-#[entry]
-fn main() -> ! {
-    // pulling peripherals
-    let peripherals = stm32::Peripherals::take().unwrap();
-    let cortex_peripherals = cortex_m::Peripherals::take().unwrap();
-    // using rcc
-    let rcc = peripherals.RCC.constrain();
+#[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        bluetooth_tx: Tx<stm32::USART1>,
+        bluetooth_rx: Rx<stm32::USART1>,
+        delay: Delay,
+        buzzer_pin: PB9<Output<PushPull>>,
+    }
+    #[init()]
+    fn init(cx: init::Context) -> init::LateResources {
+        // pulling peripherals
+        let peripherals: stm32::Peripherals = cx.device;
+        let cortex_peripherals = cortex_m::Peripherals::take().unwrap();
+        // using rcc
+        let rcc = peripherals.RCC.constrain();
 
-    // clock for usart1 timiing
-    let clocks = rcc.cfgr.freeze();
-    //
-    let mut delay = Delay::new(cortex_peripherals.SYST, clocks);
+        // clock for usart1 timiing
+        let clocks = rcc.cfgr.freeze();
+        //
+        let delay = Delay::new(cortex_peripherals.SYST, clocks);
 
-    // setup gpioa for the tx and rx pins for the HC-05 bluetooth board
-    let gpioa = peripherals.GPIOA.split();
-    // setup gpiob for the button
-    let gpiob = peripherals.GPIOB.split();
+        // setup gpioa for the tx and rx pins for the HC-05 bluetooth board
+        let gpioa = peripherals.GPIOA.split();
+        // setup gpiob for the button
+        let gpiob = peripherals.GPIOB.split();
 
-    let mut buzzer_pin = gpiob.pb9.into_push_pull_output();
+        let buzzer_pin = gpiob.pb9.into_push_pull_output();
 
-    // create tx and rx pins with alternative funcction 7
-    // USART1 is found as AF07 within datasheet
-    let usart1_txd = gpioa.pa9.into_alternate_af7();
-    let usart1_rxd = gpioa.pa10.into_alternate_af7();
+        // create tx and rx pins with alternative funcction 7
+        // USART1 is found as AF07 within datasheet
+        let usart1_txd = gpioa.pa9.into_alternate_af7();
+        let usart1_rxd = gpioa.pa10.into_alternate_af7();
 
-    // setup bluetooth config
-    let bluetooth_config = config::Config {
-        baudrate: time::Bps(9600),
-        wordlength: config::WordLength::DataBits8,
-        parity: config::Parity::ParityNone,
-        stopbits: config::StopBits::STOP1,
-    };
+        // setup bluetooth config
+        let bluetooth_config = config::Config {
+            baudrate: time::Bps(9600),
+            wordlength: config::WordLength::DataBits8,
+            parity: config::Parity::ParityNone,
+            stopbits: config::StopBits::STOP1,
+        };
 
-    // setup usart with tx and rx pins, along with bus and clocks
-    let usart1 = Serial::usart1(
-        peripherals.USART1,
-        (usart1_txd, usart1_rxd),
-        bluetooth_config,
-        clocks,
-    )
-    .unwrap();
+        // setup usart with tx and rx pins, along with bus and clocks
+        let usart1 = Serial::usart1(
+            peripherals.USART1,
+            (usart1_txd, usart1_rxd),
+            bluetooth_config,
+            clocks,
+        )
+        .unwrap();
 
-    // split out the tx and rx communication of the bluetooth
-    let (_usart1_tx, mut usart1_rx) = usart1.split();
+        peripherals
+            .SYSCFG
+            .exticr3
+            .write(|w| unsafe { w.exti9().bits(1) });
 
-    loop {
-        // create empty data array to put read data into
+        // split out the tx and rx communication of the bluetooth
+        let (bluetooth_tx, bluetooth_rx) = usart1.split();
+        init::LateResources {
+            bluetooth_tx,
+            bluetooth_rx,
+            delay,
+            buzzer_pin,
+        }
+    }
+    #[task(binds = USART1, resources = [buzzer_pin, delay, bluetooth_rx])]
+    fn usart1_interrupt(ctx: usart1_interrupt::Context) {
         let mut data = [0u8; 32];
 
         // make sure at least 4 bytes are received before going forward
         for x in 0..4 {
             // Wait for signal to come from sender and put the read into data vector
-            data[x] = block!(usart1_rx.read()).unwrap();
+            data[x] = block!(ctx.resources.bluetooth_rx.read()).unwrap();
         }
-
-        // run buzzer signal
-        buzz(&mut buzzer_pin, 300, &mut delay, 500);
-        delay.delay_ms(500u32);
-        buzz(&mut buzzer_pin, 1000, &mut delay, 500);
-        delay.delay_ms(500u32);
-        buzz(&mut buzzer_pin, 200, &mut delay, 500);
+        buzz(ctx.resources.buzzer_pin, 1000, ctx.resources.delay, 500);
     }
-}
+};
 
-fn buzz(
-    pin: &mut gpio::gpiob::PB9<gpio::Output<gpio::PushPull>>,
-    hz: u32,
-    delay: &mut Delay,
-    duration: u32,
-) -> () {
+pub fn buzz(pin: &mut PB9<Output<PushPull>>, hz: u32, delay: &mut Delay, duration: u32) -> () {
     // start with pin in low to make sure
     pin.set_low().unwrap();
 
