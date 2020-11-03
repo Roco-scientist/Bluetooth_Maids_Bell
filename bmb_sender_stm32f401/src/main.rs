f3e2ac64ccb3a5c22564016b2e509c640a15b54c
Rory Coffey <coffeyrt@gmail.com>
2020-11-02 23:28:01 -0500

    moved everything around for rtic

--- bmb_sender_stm32f401/src/main.rs
+++ bmb_sender_stm32f401/src/main.rs
@@ -7,80 +7,82 @@ use panic_itm as _;
 #[cfg(not(debug_assertions))]
 use panic_abort as _;
 
-use cortex_m_rt::entry;
+use rtic::app;
 use stm32f4xx_hal::{
     block,
+    gpio::{gpiob::PB10, Input, PullDown},
     prelude::*,
-    serial::{config, Serial},
+    serial::{config, Rx, Serial, Tx},
     stm32, time,
 };
 
-#[entry]
-fn main() -> ! {
-    // pulling peripherals
-    let peripherals = stm32::Peripherals::take().unwrap();
-    // using rcc
-    let rcc = peripherals.RCC.constrain();
+#[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        bluetooth_tx: Tx<stm32::USART1>,
+        bluetooth_rx: Rx<stm32::USART1>,
+        button: PB10<Input<PullDown>>,
+    }
+    #[init()]
+    fn init(cx: init::Context) -> init::LateResources {
+        let device: stm32::Peripherals = cx.device;
+        // pulling peripherals
+        let peripherals = stm32::Peripherals::take().unwrap();
+        // using rcc
+        let rcc = device.RCC.constrain();
 
-    // clock for usart1 timiing
-    let clocks = rcc.cfgr.freeze();
+        // clock for usart1 timiing
+        let clocks = rcc.cfgr.freeze();
 
-    // setup gpioa for the tx and rx pins for the HC-05 bluetooth board
-    let gpioa = peripherals.GPIOA.split();
-    // setup gpiob for the button
-    let gpiob = peripherals.GPIOB.split();
+        // setup gpioa for the tx and rx pins for the HC-05 bluetooth board
+        let gpioa = peripherals.GPIOA.split();
+        // setup gpiob for the button
+        let gpiob = peripherals.GPIOB.split();
 
-    // create pull down input button pin on pb2
-    let button = gpiob.pb10.into_pull_down_input();
+        // create pull down input button pin on pb2
+        let button = gpiob.pb10.into_pull_down_input();
 
-    // create tx and rx pins with alternative funcction 7
-    // USART1 is found as AF07 within datasheet
-    let usart1_txd = gpioa.pa9.into_alternate_af7();
-    let usart1_rxd = gpioa.pa10.into_alternate_af7();
+        // set pb10 as an external interrupt
+        // TODO Fix this
+        device.SYSCFG.exticr3.write(|w| w.exti10().bits(1));
 
-    // setup bluetooth config
-    let bluetooth_config = config::Config {
-        baudrate: time::Bps(9600),
-        wordlength: config::WordLength::DataBits8,
-        parity: config::Parity::ParityNone,
-        stopbits: config::StopBits::STOP1,
-    };
+        // create tx and rx pins with alternative funcction 7
+        // USART1 is found as AF07 within datasheet
+        let usart1_txd = gpioa.pa9.into_alternate_af7();
+        let usart1_rxd = gpioa.pa10.into_alternate_af7();
 
-    // setup usart with tx and rx pins, along with bus and clocks
-    let usart1 = Serial::usart1(
-        peripherals.USART1,
-        (usart1_txd, usart1_rxd),
-        bluetooth_config,
-        clocks,
-    )
-    .unwrap();
+        // setup bluetooth config
+        let bluetooth_config = config::Config {
+            baudrate: time::Bps(9600),
+            wordlength: config::WordLength::DataBits8,
+            parity: config::Parity::ParityNone,
+            stopbits: config::StopBits::STOP1,
+        };
 
-    // split out the tx and rx communication of the bluetooth
-    let (mut usart1_tx, _usart1_rx) = usart1.split();
+        // setup usart with tx and rx pins, along with bus and clocks
+        let usart1 = Serial::usart1(
+            peripherals.USART1,
+            (usart1_txd, usart1_rxd),
+            bluetooth_config,
+            clocks,
+        )
+        .unwrap();
 
-    // used later to display whether or not a signal was received
-    loop {
-        // Below is for debugging
-        //if button.is_low().unwrap() {
-        //    usart1_tx.bwrite_all(&b"LOW"[..]).unwrap();
-        //    usart1_tx.bflush().unwrap();
-        //} else {
-        //    if button.is_high().unwrap() {
-        //        usart1_tx.bwrite_all(&b"HIGH"[..]).unwrap();
-        //        usart1_tx.bflush().unwrap();
-        //    } else {
-        //        usart1_tx.bwrite_all(&b"NONE"[..]).unwrap();
-        //        usart1_tx.bflush().unwrap();
-        //    }
-        //}
+        // split out the tx and rx communication of the bluetooth
+        let (mut bluetooth_tx, bluetooth_rx) = usart1.split();
+        init::LateResources {
+            bluetooth_tx,
+            bluetooth_rx,
+            button,
+        }
+    }
 
-        // While button is not pressed, wait
-        while button.is_low().unwrap() {}
-
+    #[task(binds = EXTI3_10, resources = [button, bluetooth_tx])]
+    fn exti_3_10_interrupt(ctx: exti_3_10_interrupt::Context) {
         // When button is pressed send a BUZZ signal
         for byte in b"BUZZ" {
-            block!(usart1_tx.write(*byte)).unwrap();
+            block!(ctx.resources.bluetooth_tx.write(*byte)).unwrap();
         }
-        block!(usart1_tx.flush()).unwrap();
+        block!(ctx.resources.bluetooth_tx.flush()).unwrap();
     }
-}
+};
